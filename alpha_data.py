d0d9e37f7c51c773e66b6ae4045868389c389a3b
hubertpie <hubert.pietron95@gmail.com>
2020-11-14 17:43:52 +0100

    added chart.js library and created simple chart for data visualisation

--- alpha_data.py
+++ alpha_data.py
@@ -5,8 +5,9 @@ import secrets
 ts = ForeignExchange(key=secrets.API_KEY)
 
 def get_data(curr_from, curr_to):
-	return ts.get_currency_exchange_daily(from_symbol=curr_from, to_symbol=curr_to, outputsize='full')
-
+	data = ts.get_currency_exchange_daily(from_symbol=curr_from, to_symbol=curr_to, outputsize='compact')
+	return [[x, data[0][x]['4. close']] for x in data[0]]
+	
 def currency_list():
 	with open('physical_currency_list.csv') as csvfile:
 		return [curr for curr in csv.reader(csvfile, delimiter=',')]
--- application.py
+++ application.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 def index():
 	currency_list = alpha_data.currency_list()
 	if request.method == 'POST':
-		print(request.form['currency1'])
-		print(request.form['currency2'])
 		data = alpha_data.get_data(request.form['currency1'], request.form['currency2'])
-		return render_template('index.html', data=data, currency_list=currency_list)
+		dates = [x[0] for x in data]
+		ex_rate = [y[1] for y in data]
+		return render_template('index.html', currency_list=currency_list, dates=dates, ex_rate=ex_rate)
 
 	return render_template('index.html', currency_list=currency_list)
