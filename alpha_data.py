21a3e1621add2ea5ddcb3e8541da8b43d33e5dc8
hubertpie <hubert.pietron95@gmail.com>
2020-11-16 19:03:48 +0100

    changed functions names, added current exchange rate

--- alpha_data.py
+++ alpha_data.py
@@ -4,10 +4,14 @@ import secrets
 
 ts = ForeignExchange(key=secrets.API_KEY)
 
-def get_data(curr_from, curr_to):
+def get_historical_exchange_rate(curr_from, curr_to):
 	data = ts.get_currency_exchange_daily(from_symbol=curr_from, to_symbol=curr_to, outputsize='compact')
 	return [[x, data[0][x]['4. close']] for x in data[0]]
 	
 def currency_list():
 	with open('physical_currency_list.csv') as csvfile:
 		return [curr for curr in csv.reader(csvfile, delimiter=',')]
+
+def get_current_exchange_rate(curr_from, curr_to):
+	data = ts.get_currency_exchange_rate(from_currency=curr_from, to_currency=curr_to)
+	return data[0]['5. Exchange Rate']
--- application.py
+++ application.py
@@ -9,9 +9,10 @@ app = Flask(__name__)
 def index():
 	currency_list = alpha_data.currency_list()
 	if request.method == 'POST':
-		data = alpha_data.get_data(request.form['currency1'], request.form['currency2'])
-		dates = [x[0] for x in data]
-		ex_rate = [y[1] for y in data]
-		return render_template('index.html', currency_list=currency_list, dates=dates, ex_rate=ex_rate)
+		historical_exchange_rate = alpha_data.get_historical_exchange_rate(request.form['currency1'], request.form['currency2'])
+		current_exchange_rate = alpha_data.get_current_exchange_rate(request.form['currency1'], request.form['currency2'])
+		dates = [x[0] for x in historical_exchange_rate]
+		ex_rate = [y[1] for y in historical_exchange_rate]
+		return render_template('index.html', currency_list=currency_list, dates=dates, ex_rate=ex_rate, current_exchange_rate=current_exchange_rate)
 
 	return render_template('index.html', currency_list=currency_list)
